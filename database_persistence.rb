7ff70e7aa78fb73ea88049141ae2a5189b502091
Abbie Beck <abbie.beck@gmail.com>
2025-12-04 17:08:28 -0500

    changes

--- database_persistence.rb
+++ database_persistence.rb
@@ -3,7 +3,8 @@ require "pg"
 class DatabasePersistence
   def initialize(logger)
     @db = if
-  Sinatra::Base.production?        PG.connect(ENV['DATABASE_URL'])
+  Sinatra::Base.production
+  PG.connect(ENV['DATABASE_URL'])
       else
         PG.connect(dbname: "todos")
       end
